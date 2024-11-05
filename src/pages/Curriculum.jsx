3cc5c5447b478cd59c4f8d3aac4ba5bb1197be11
Naveen VJ <vjnaveen2005@gmail.com>
2024-11-05 22:29:37 +0530

    feat : Curriculum page added

--- src/pages/Curriculum.jsx
+++ src/pages/Curriculum.jsx
@@ -1,15 +1,59 @@
 import React from 'react'
 import './style.css'
+import './Curriculum.css'
 
 function Curriculum() {
   return (
-    <section>
-            <div className="container">
-                <div className="content">
-                    <h2>Curriculum</h2>
-                </div>
+    <>
+
+      <section className='flex-curriculum'>
+
+        <div className="card-container">
+          <div className="card-curriculum">
+            <div className="text">
+              <h2>B.Tech</h2>
+              <h3>AY 2014-15</h3>
+              <a className='bt' href='https://drive.google.com/file/d/1yy-ZL45Lz0vz2wxpkeLvbThfpamyRkDr/view' target='blank'>Download</a>
             </div>
-        </section>
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-curriculum">
+            <div className="text">
+              <h2>B.Tech</h2>
+              <h3>AY 2018-19</h3>
+              <a className='bt' href='https://drive.google.com/file/d/1x-SvfsEK8jm_-eqw-8tI8gk3oLvp7kMH/view' target='blank'>Download</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-curriculum">
+            <div className="text">
+              <h2>B.Tech</h2>
+              <h3>AY 2020-21</h3>
+              <a className='bt' href='https://drive.google.com/file/d/1ZYF24gmt_Y9S2L2ZMpBumnif0yUbYLWO/view' target='blank'>Download</a>
+            </div>
+          </div>
+        </div>
+
+
+      </section>
+      
+      <section className='flex-curriculum'>
+      <div className="card-container">
+          <div className="card-curriculum">
+            <div className="text">
+              <h2>M.Tech</h2>
+              <h3>AY 2020-21</h3>
+              <a className='bt' href='https://cms.ptuniv.edu.in/file_contents/syllabus/17_MT_EE_ED_2021.pdf' target='blank'>Download</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
   )
 }
 
